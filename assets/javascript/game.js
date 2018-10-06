05c3503304b8b3719308a341ce634027e6749011
Max <maxniederluecke@gmail.com>
2018-10-05 22:36:59 -0500

    Added HTML and some CSS

---
+++ assets/javascript/game.js
@@ -0,0 +1,7 @@
+var names = ["brown", "coyle", "ek", "fehr", "foligno", "granlund", "greenway", "hendricks", "koivu", "kunin", "niederreiter", "parise", "staal", "zucker", "brodin", "dumba", "pateryn", "prosser", "seeler", "spurgeon", "suter", "dubnyk", "stalock"];
+var wordLength = names.length;
+var currentWord = "";
+var wins = 0;
+var guesses = 10;
+var guessed = [];
+
